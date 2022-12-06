462cacbd0336584747cb68d208a5b6641b5eeb22
Marcelo Hora <marcelohora@Marcelos-MacBook-Pro.local>
2022-12-06 20:16:43 -0300

    TrocadeDados

---
+++ Desafio_Troca.js
@@ -0,0 +1,30 @@
+/* O objetivo desse desafio é fazer a troca das
+ variaveis "a" e "b", somente invertendo seus fatores.*/
+
+{// 1ºmaneira: 
+let a =7;
+let b =94;
+
+    // ...
+    a=94;
+    // ...
+
+    // ...
+    b=7;
+    // ...
+
+console.log(a);
+console.log(b);
+}
+console.log(" ")
+{// 2ºmaneira: 
+let a =7;
+let b =94;
+
+let temp= a
+a=b;
+b= temp;
+
+console.log(a);
+console.log(b);
+}
