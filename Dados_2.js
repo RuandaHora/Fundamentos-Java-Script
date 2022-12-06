1b851b4dcc4f5809b9df4f92b67e013503e98453
Marcelo Hora <marcelohora@Marcelos-MacBook-Pro.local>
2022-12-05 21:00:01 -0300

    first commit

---
+++ Dados_2.js
@@ -0,0 +1,21 @@
+let preco= 19.90;
+let desconto=0.4;
+
+{// Usnaod com valores literais: 
+    console.log(19.90*0.6);
+}
+
+console.log(preco* (1 - desconto));
+
+{// Usando com variaveis: 
+    let precoComDesconto= preco* (1 - desconto)
+console.log(precoComDesconto)
+} 
+
+{let nome= "Caderno";
+let categoria="Papelaria";
+//    Strings (Texto) -> Sequência de símbolos.
+console.log("Categoria: "+ categoria 
++ ", Nome: "+ nome
++ " ,Preço: "+ preco
++ " ,Desconto: "+ desconto)}
