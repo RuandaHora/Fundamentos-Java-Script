3a0e2215b7efd7228160eee94398d72909c161aa
Ruan da Hora <ruanpablosnake@gmail.com>
2022-12-25 23:07:29 -0300

    TestCommit

---
+++ TestCommit.js
@@ -0,0 +1,17 @@
+  //Preço das Camisas
+    let Brasil= 120.0;
+    let Espanha= 100.0;
+    let Argentina= 100.0;
+
+    //Serviços 
+    let ImpostoAduaneiro= 50.0;
+    let frete= 20.0;
+    let gorjeta= "*opicional*";
+
+console.log("Camisas das seleções")
+console.log()
+console.log("Tabela de preços: ")
+console.log()
+console.log("Camisa do Brasil: R$ " + Brasil);
+console.log("Camisa da Espanha: R$ " + Espanha + "      NO CALOTEIROS");
+console.log("Camisa da Argentina: R$ " + Argentina)
